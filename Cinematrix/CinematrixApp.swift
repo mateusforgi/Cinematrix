1bd2e127f4a44295e4f29cee44046a877cb81389
Mateus Forgiarini da Silva <mateusforgi@gmail.com>
2024-03-22 12:04:35 -0300

    Start Up

---
+++ Cinematrix/CinematrixApp.swift
@@ -0,0 +1,20 @@
+//
+//  CinematrixApp.swift
+//  Cinematrix
+//
+//  Created by Mateus on 22/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct CinematrixApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
